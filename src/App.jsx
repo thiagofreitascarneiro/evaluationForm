a3513a4fed600b8500854380e5e9fb2b806c2a71
Thiago <thiagofreitascarneiro@hotmail.com>
2023-03-05 17:02:07 -0300

    creating header and sidebar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './global.css';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css';
+// import { Post } from './components/Sidebar';
+
+
+function App() {
+
+  return (
+   <div>
+    <Header/>
+
+    <div className={styles.wrapper}>
+    <Sidebar />
+    <main>
+
+    </main>
+    </div>
+   </div>
+  )
+}
+
+export default App
