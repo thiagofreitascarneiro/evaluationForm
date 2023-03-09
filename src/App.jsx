be86a7a0e6878585f191451b22f11ba7a94ec3a0
Thiago <thiagofreitascarneiro@hotmail.com>
2023-03-09 16:54:54 -0300

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,38 @@ import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/thiagofreitascarneiro.png',
+      name: 'Thiago Carneiro',
+      role: 'CTO @Encinterativa'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋', },
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type: 'link', content: 'jane.design/doctorcare'},
 
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayke BBrito',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋', },
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type: 'link', content: 'jane.design/doctorcare'},
+
+    ],
+    publishedAt: new Date('2023-03-09 15:00:00'),
+  },
+]
 
 function App() {
 
@@ -16,7 +47,15 @@ function App() {
     <div className={styles.wrapper}>
     <Sidebar />
     <main>
-      <Post />
+      {posts.map(post => {
+        return (
+        <Post
+          author={post.author}
+          content={post.content}
+          publishedAt={post.publishedAt}
+        />
+        )
+      })}
 
     </main>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
     return (
@@ -8,11 +9,10 @@ export function Sidebar() {
           <img 
             src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=280&q=50"
             alt=""
+            className={styles.cover}
           /> 
           <div className={styles.profile}>
-            <img className={styles.avatar} 
-                src="https://github.com/thiagofreitascarneiro.png" 
-            />
+            <Avatar src="https://github.com/thiagofreitascarneiro.png" />
 
             <strong>THiago Carneiro</strong>  
             <span>Web Developer</span>
