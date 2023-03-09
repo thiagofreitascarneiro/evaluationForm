29b97725bfe95a7d95800c76f9ff56a281f7eef1
Thiago <thiagofreitascarneiro@hotmail.com>
2023-03-09 17:19:04 -0300

    creating state in the post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,15 @@ import styles from './Post.module.css';
 import Comment from './Comment'
 import { Avatar } from './Avatar';
 
+import { useState } from 'react';
+
+
 export function Post({ author, publishedAt, content}) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'",{
         locale: ptBR,
     })
@@ -14,6 +22,14 @@ export function Post({ author, publishedAt, content}) {
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+        
+        setComments([...comments, comments.length +  1]);
+
+        console.log(comments)
+    }
 
     return (
         <article className={styles.post}>
@@ -42,7 +58,7 @@ export function Post({ author, publishedAt, content}) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
@@ -55,9 +71,10 @@ export function Post({ author, publishedAt, content}) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
+               
             </div>
 
         </article>  
