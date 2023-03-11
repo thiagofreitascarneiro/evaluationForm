43c4dfabc1ab1209394e5e5b13e985d2c3f204bf
Thiago <thiagofreitascarneiro@hotmail.com>
2023-03-11 17:52:49 -0300

    validated comment in the form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,7 +33,12 @@ export function Post({ author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
     }
 
     function deleteComment(commentToDelete) {
@@ -78,6 +83,8 @@ export function Post({ author, publishedAt, content}) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
                     <button type="submit">Comentar</button>
